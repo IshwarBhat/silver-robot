6f3029b46585414ddfbca54846aa1a48f50357b8
IshwarBhat <ishwrbhat@gmail.com>
2016-03-10 18:03:34 +0530

    Update 03. PL-SQL Logic Flow.sql

--- 03. PL-SQL Logic Flow.sql
+++ 03. PL-SQL Logic Flow.sql
@@ -41,8 +41,41 @@ ELSIF v_hours_worked < 20 THEN
   dbms_output.put_line('You are part-time help');
 END IF;
 
+END;
 
+/*
+- CASE
+       - Alternative to IF/THEN/ELSE
+       -Two formats:
+            - Return a variable
+            - Runs commands
+      
+*/
+-- Lab
+set serveroutput on;
+DECLARE
 
+v_hours_worked NUMBER(3) := &Hours_Worked;
+
+BEGIN
+
+CASE
+ WHEN v_hours_worked > 40 THEN
+  dbms_output.put_line('You worked overtime');
+ WHEN v_hours_worked = 40 THEN
+  dbms_output.put_line('You worked a full week');
+ WHEN v_hours_worked BETWEEN 20 and 40 THEN
+  dbms_output.put_line('Have you been sick?');
+ WHEN v_hours_worked < 20 THEN
+  dbms_output.put_line('You are part-time help');
+ ELSE
+  dbms_output.put_line('You did not enter a valid number of hours');
+ END CASE;
+  
+END IF;
+
+END;
+/
 
 
 
