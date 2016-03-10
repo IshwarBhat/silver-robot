b157395b4cb13f4bb0705bf6c1471711d4362db4
IshwarBhat <ishwrbhat@gmail.com>
2016-03-10 17:41:45 +0530

    Create 03. PL-SQL Logic Flow.sql

---
+++ 03. PL-SQL Logic Flow.sql
@@ -0,0 +1,14 @@
+/*
+- Allows for conditional flow of logic
+- Can nest as deep as you like
+- Conditions look like WHERE clause of SQL
+=, !=, <, >, BETWEEN, calcualtion, IS NULL,
+IS NOT NULL, etc
+
+IF <condition> THEN statement[s];
+[ELSIF <condition> THEN statement[s];]
+[ELSE statement[s];]
+END IF;
+
+*/
+
