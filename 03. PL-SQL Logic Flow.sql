b2572b9c050eb4dc1db337237f147c8377220a32
IshwarBhat <ishwrbhat@gmail.com>
2016-03-10 18:14:36 +0530

    Update 03. PL-SQL Logic Flow.sql

--- 03. PL-SQL Logic Flow.sql
+++ 03. PL-SQL Logic Flow.sql
@@ -79,5 +79,24 @@ END;
 
 
 
+/*
+LOOPS
+*/
+--Lab
+set serveroutput on
+DECLARE
+
+v_count NUMBER(3) := 0;
+
+BEGIN
+
+FOR v_count IN 1 .. 5
+LOOP
+      dbms_output.put_line('Ishwar Bhat');
+END LOOP;
+
+END;
+/
+
 
 
