0274ae1f62b92c023d9b287b16fa2ccba061c5ef
IshwarBhat <ishwrbhat@gmail.com>
2016-03-11 21:26:21 +0530

    Update 06. Exception Handling.sql

--- 06. Exception Handling.sql
+++ 06. Exception Handling.sql
@@ -31,3 +31,82 @@ when TOO_MAY_ROWS then
 	dbms_output.put_line('Too many rows found');
 END;
 /
+-- Implicit and Explicit exceptions
+/*
+Implicit: Automatic e.g. NO_DATA_FOUND, ZERO_DIVIDE, TOO_MANY_ROWS etc
+*/
+
+DECLARE
+	v_ename varchar2(20);
+
+	my_too_many_rows EXCEPTION;
+
+BEGIN
+	SELECT 	ename
+	into V_ENAME
+	FROM emp;
+
+DBMS_OUTPUT.PUT_LINE('Employee: ' || v_ename);
+EXCEPTION
+when NO_DATA_FOUND then
+	dbms_output.put_line('No Data Found');
+when OTHERS then
+dbms_output.put_line('Oracle Error: ' || SQLCODE || ' ' || SQLERRM);
+END;
+/
+--Oracle Error: -1422 ORA-01422: exact fetch returns more than requested number of rows
+
+DECLARE
+	v_ename varchar2(20);
+
+	my_too_many_rows EXCEPTION;
+	PRAGMA EXCEPTION_INT(my_too_many_rows, -1422);
+BEGIN
+	SELECT 	ename
+	into V_ENAME
+	FROM emp;
+
+DBMS_OUTPUT.PUT_LINE('Employee: ' || v_ename);
+EXCEPTION
+when NO_DATA_FOUND then
+	dbms_output.put_line('No Data Found');
+when my_too_many_rows then
+	dbms_output.put_line('Too many rows encountered...');
+when OTHERS then
+dbms_output.put_line('Oracle Error: ' || SQLCODE || ' ' || SQLERRM);
+END;
+/
+
+--Oracle Built-In Features
+--dbms_utility.format_error_backtrace
+--10g+ features: dbms_utility.format_call_stack; dbms_utility.format_error_stack
+
+-- User-defined exceptions
+/*
+- Need to be explicitly declared and raised
+*/
+
+IF v_sal > 2500 then
+	RAISE MAKES_TOO_MUCH;
+END IF;
+
+EXCEPTION
+when MAKES_TOO_MUCH then
+	dbms_output.put_line('This person is overpaid!');
+
+--Application defined exception
+DECLARE
+	MAKES_TOO_MUCH EXCEPTION;
+
+BEGIN
+IF v_sal > 2500 then
+	RAISE MAKES_TOO_MUCH;
+END IF;
+
+EXCEPTION
+when MAKES_TOO_MUCH then
+	RAISE_APPLICATION_ERROR(-20000, 'This person is overpaid!')
+
+END;
+
+-- Lab
